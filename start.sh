2210a119e1d8f653ef7b5948bde1af8327d8d4f1
krishan041 <krishandara04@gmail.com>
2025-01-21 21:13:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
-pip3 install -r requirements.txt
+#!/bin/bash -c pip3 install -U -r requirements.txtpip3 install -r requirements.txt
+
+# Make the script executable
+chmod +x start.sh
+
+# Activate virtual environment if needed
+# source venv/bin/activate
+
+# Export environment variables if needed
+# export BOT_TOKEN=""
+# export API_ID="27536109"
+# export API_HASH="b84d7d4dfa33904d36b85e1ead16bd63"
+
+# Start the bot
+python3 main.py
 
 # Make the script executable
 chmod +x start.sh
