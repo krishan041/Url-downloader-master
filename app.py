5941e0db1f0e2fa3f95aed986c7acd128e869cb0
krishan041 <krishandara04@gmail.com>
2025-01-23 00:52:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'GrryMatters'
-
+def home():
+    return "URL Downloader is running successfully!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
