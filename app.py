d752338a571cff79f4db2a7b5153893b8e2674e8
krishan041 <krishandara04@gmail.com>
2025-01-26 18:54:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+FROM python:3.10.8-slim-buster
+RUN apt-get update -y && apt-get upgrade -y \
+    && apt-get install -y --no-install-recommends gcc libffi-dev musl-dev ffmpeg aria2 python3-pip \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY . /app/
+WORKDIR /app/
+RUN pip3 install --no-cache-dir --upgrade --requirement requirements.txt
+CMD gunicorn app:app & python3 main.py
